af80b34f413db36a8bcb807846355d35e2dc6a72
raquellopes3 <raquelsilveriolopes@gmail.com>
2025-11-06 00:04:47 -0300

    foi adicionado o accordion do bootstrap e o audio com js

---
+++ js/pinkfloyd.js
@@ -0,0 +1,12 @@
+function playAudio() {
+    let x= document.getElementById("myAudio");
+    x.play();
+}
+
+
+function pauseAudio() {
+    let x= document.getElementById("myAudio");
+    x.pause();
+}
+
+
